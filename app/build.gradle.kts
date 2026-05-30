017b7cb4cf9f425e5566dfd82d13e31afb21c79a
hexman1999 <mohamedraesa@gmail.com>
2026-05-30 17:30:29 +0300

    Enable R8 + resource shrinking for release, add ABI splits

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,10 +17,21 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("arm64-v8a", "armeabi-v7a")
+            isUniversalApk = true
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
